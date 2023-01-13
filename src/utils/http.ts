b3fb742319e690331b0f4c3ba733d2d219eae276
rain <3100406687@qq.com>
2023-01-13 09:55:10 +0800

    使用feuch抽象通用http方法

---
+++ src/utils/http.ts
@@ -0,0 +1,42 @@
+import qs from "qs"
+import * as auth from 'auth-provider'
+
+const apiUrl = process.env.REACT_APP_API_URL
+
+/**
+ * 请求封装
+ * axios可以再状态码不为2xx的时候抛出异常
+ */
+interface Config extends RequestInit {
+    token?: string
+    data?: object
+}
+
+export const http = async (endpoint: string, {data, token, headers, ...customerConfig}: Config) => {
+    const config = {
+        method: 'GET',
+        headers: {
+            Authorization: token ? `Bearer ${token}` : '',
+            "Content-Type": data ? 'application/json' : ''
+        },
+        ...customerConfig
+    }
+    if (config.method.toUpperCase() === 'GET') {
+        endpoint += `?${qs.stringify(data)}`
+    } else {
+        config.body = JSON.stringify(data || {})
+    }
+    return window.fetch(`${apiUrl}/${endpoint}`, config).then(async response => {
+        if (response.status === 401) {
+            await auth.logout()
+            window.location.reload() // 重新刷新页面
+            return Promise.reject({message: '请重新登录'})
+        }
+        const data = await response.json()
+        if (response.ok) {
+            return data
+        } else {
+            return Promise.reject(data)
+        }
+    })
+}
