fcf07257cdf095d3028cce19ad5ffc6e7fbc5fd0
rain <3100406687@qq.com>
2023-03-29 21:21:58 +0800

    使用redux-toolkit

--- src/screens/project-list/list.tsx
+++ src/screens/project-list/list.tsx
@@ -11,74 +11,79 @@ export const List = ({ users, ...props }: ListProps) => {
   const { mutate } = useEditProject()
   const pinProject = (id: number) => (pin: boolean) =>
     mutate({ id, pin }).then(props.refresh)
+
+  //
+  //
   return (
-    <Table
-      rowKey={'id'}
-      pagination={false}
-      columns={[
-        {
-          title: <Pin checked={true} disabled={true} />,
-          render(value, project) {
-            return (
-              <Pin
-                checked={project.pin}
-                onCheckedChange={pinProject(project.id)}
-              />
-            )
+    <div>
+      <Table
+        rowKey={'id'}
+        pagination={false}
+        columns={[
+          {
+            title: <Pin checked={true} disabled={true} />,
+            render(value, project) {
+              return (
+                <Pin
+                  checked={project.pin}
+                  onCheckedChange={pinProject(project.id)}
+                />
+              )
+            },
           },
-        },
-        {
-          title: '名称',
-          sorter: (a, b) => a.name.localeCompare(b.name),
-          render(value, project) {
-            return <Link to={String(project.id)}>{project.name}</Link>
+          {
+            title: '名称',
+            sorter: (a, b) => a.name.localeCompare(b.name),
+            render(value, project) {
+              return <Link to={String(project.id)}>{project.name}</Link>
+            },
           },
-        },
-        {
-          title: '部门',
-          dataIndex: 'organization',
-        },
-        {
-          title: '负责人',
-          render(value, project) {
-            return (
-              <span>
-                {users.find((user) => user.id === project.personId)?.name ||
-                  '未知'}
-              </span>
-            )
+          {
+            title: '部门',
+            dataIndex: 'organization',
           },
-        },
-        {
-          title: '创建时间',
-          render(value, project) {
-            return (
-              <span>
-                {project.created
-                  ? dayjs(project.created).format('YYYY-MM-DD')
-                  : '无'}
-              </span>
-            )
+          {
+            title: '负责人',
+            render(value, project) {
+              return (
+                <span>
+                  {users.find((user) => user.id === project.personId)?.name ||
+                    '未知'}
+                </span>
+              )
+            },
           },
-        },
-        {
-          title: '更多',
-          render(value, project) {
-            const items: MenuProps['items'] = [
-              {
-                key: '1',
-                label: props.projectButton,
-              },
-            ]
-            return (
-              <Dropdown menu={{ items }}>
-                <ButtonNoPadding type="link">...</ButtonNoPadding>
-              </Dropdown>
-            )
+          {
+            title: '创建时间',
+            render(value, project) {
+              return (
+                <span>
+                  {project.created
+                    ? dayjs(project.created).format('YYYY-MM-DD')
+                    : '无'}
+                </span>
+              )
+            },
           },
-        },
-      ]}
-      {...props}
-    />
+          {
+            title: '更多',
+            render(value, project) {
+              const items: MenuProps['items'] = [
+                {
+                  key: '1',
+                  label: props.projectButton,
+                },
+              ]
+              return (
+                <Dropdown menu={{ items }}>
+                  <ButtonNoPadding type="link">...</ButtonNoPadding>
+                </Dropdown>
+              )
+            },
+          },
+        ]}
+        {...props}
+      />
+    </div>
   )
 }
